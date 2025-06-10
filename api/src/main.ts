dbe171d8d3a473c7f41d1965b03e2ccbb283f619
indunil-jay <indunil.jayz@gmail.com>
2025-06-10 23:41:07 +0530

    feat : add global pipes for validate and transform incoming all requests

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DatabaseDriver } from './common/enums/database-drivers.enum';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule.register({ driver: DatabaseDriver.ORM }),
   );
+  // Enable global request validation and transformation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove unexpected properties
+      forbidNonWhitelisted: true, // Reject requests with unexpected properties
+      transform: true, // Convert payloads to DTO instances and types
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
