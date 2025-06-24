7222d16e9bd61d0c6cbed99e3f2ae603ee4f3ee8
indunil-jay <indunil.jayz@gmail.com>
2025-06-24 07:22:54 +0530

    feat(category): create domain model

---
+++ api/src/categories/domain/category.ts
@@ -0,0 +1,12 @@
+export class Category {
+  public name: string;
+  public slug: string;
+  public description: string;
+  public createdAt: Date;
+  public parent?: Category;
+  constructor(public readonly id: string) {}
+
+  public isSubcategory(): boolean {
+    return !!this.parent;
+  }
+}
